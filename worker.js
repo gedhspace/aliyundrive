23fd5900cc72927c7d9cfe2236928d577b7d0363
Gedh-StuffyWalk <103632788+gedhspace@users.noreply.github.com>
2025-03-04 19:42:52 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 
-var refresh_token="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIxMDBlNjA0ZmM4YzM0MWVkOTJiODI3YjM5YmMxOWZkNyIsImF1ZCI6Ijc2OTE3Y2NjY2Q0NDQxYzM5NDU3YTA0ZjYwODRmYjJmIiwiZXhwIjoxNzQ4MDY5MjEzLCJpYXQiOjE3NDAyOTMyMTMsImp0aSI6IjJlNDk4NDk3Y2Y3NzQxNWY5OTcyNDA0NmY3NmRkM2UyIn0.ZDicdlXk8bL5OhI7bYGXHXF-HOeLMPHJCq3agxhAAgdVExmidpxwVD5Ne_StBrUNlO4EJHTCy52wkV45dHl2dw";
-var drive_id="604760443";
+var refresh_token="";
+var drive_id="";
 
 export default {
   async fetch(request, env, ctx) {
